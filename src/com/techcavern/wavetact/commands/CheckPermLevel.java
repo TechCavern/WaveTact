84887906d04e88090732783998e0c6cbf7aaf169
julian zhou <jzhou2oo6@me.com>
2014-06-15 11:44:12 -0400

    Finalized PermRegistry

--- src/com/techcavern/wavetact/commands/CheckPermLevel.java
+++ src/com/techcavern/wavetact/commands/CheckPermLevel.java
@@ -4,9 +4,12 @@ import org.pircbotx.hooks.events.MessageEvent;
 import com.techcavern.wavetact.utils.*;
 public class CheckPermLevel extends ListenerAdapter {
 	        public void onMessage(MessageEvent event) {
-	                if (event.getMessage().equalsIgnoreCase(GeneralRegistry.getCommandChar()+"cperm")){
-	                	event.respond(event.getUser().toString());	        }
+	                if ((GeneralRegistry.getCommandChar()+"cperm").equalsIgnoreCase(event.getMessage().toString())){
+	                	event.respond(event.getUser().toString());
+	                	event.respond("hi");}
+	             
 	}
+	        
 }
 
 
--- src/com/techcavern/wavetact/utils/PermRegistry.java
+++ src/com/techcavern/wavetact/utils/PermRegistry.java
@@ -3,11 +3,28 @@ package com.techcavern.wavetact.utils;
 import java.util.Arrays;
 import java.util.List;
 
+import org.pircbotx.PircBotX;
+import org.pircbotx.User;
+import org.pircbotx.hooks.WaitForQueue;
+import org.pircbotx.hooks.events.WhoisEvent;
+
+import com.techcavern.wavetact.Main;
+
 public class PermRegistry {
-	public boolean isController (String x){
-		String[] Controllers = new String[]{"*!jztech101@techcavern.com","*!jztech101@crabhost.org"};
+	public static String getAccount(User x)
+    {
+        String u = "";
+        Main.WaveTact.sendRaw().rawLineNow("WHOIS " + x.getNick());
+        WhoisEvent<PircBotX> whois = null;
+        u = whois.getRegisteredAs();
+        return u;
+    }
+	public boolean isController (User x){
+		String v= getAccount(x);
+		String[] Controllers = new String[]{"jztech101"};
 		List <String> Controllerlist = Arrays.asList(Controllers);
-		boolean y = Controllerlist.contains(x);
+		boolean y = Controllerlist.contains(v);
 		return y;
 	}
+	
 }
