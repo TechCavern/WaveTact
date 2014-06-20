8681c5ebcc669c655eafd124d50737a004ce315e
julian zhou <jzhou2oo6@me.com>
2014-06-20 19:44:10 -0400

    Added Source Command

--- src/com/techcavern/wavetact/commands/BasicCommands.java
+++ src/com/techcavern/wavetact/commands/BasicCommands.java
@@ -8,6 +8,7 @@ public class BasicCommands extends ListenerAdapter<PircBotX> {
 	public void onMessage(MessageEvent<PircBotX> event) throws Exception{
 	   IRCUtils.onMessage("ping","pong",event.getChannel(),event.getUser(),event.getBot(),event.getMessage(),0);     
 	   IRCUtils.onMessage("pong","ping",event.getChannel(),event.getUser(),event.getBot(),event.getMessage(),0);
+	   IRCUtils.onMessage("Source","https://github.com/TechCavern/WaveTact",event.getChannel(),event.getUser(),event.getBot(),event.getMessage(),0);
 
 	}
 }            
