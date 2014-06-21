0e46475b5905348988fb3c5a7f8b2f8086acd4c2
julian zhou <jzhou2oo6@me.com>
2014-06-20 20:00:48 -0400

    Added Say & Act (Trusted)

--- src/com/techcavern/wavetact/commands/BasicChanOp.java
+++ src/com/techcavern/wavetact/commands/BasicChanOp.java
@@ -7,6 +7,7 @@ package com.techcavern.wavetact.commands;
 
 import com.techcavern.wavetact.utils.GeneralRegistry;
 import com.techcavern.wavetact.utils.IRCUtils;
+import com.techcavern.wavetact.utils.PermUtils;
 import org.pircbotx.PircBotX;
 import org.pircbotx.UserLevel;
 import org.pircbotx.hooks.ListenerAdapter;
@@ -15,23 +16,20 @@ import org.pircbotx.hooks.events.MessageEvent;
 public class BasicChanOp extends ListenerAdapter<PircBotX> {
 		public void onMessage(MessageEvent<PircBotX> event) throws Exception{
                     String[] messageParts = event.getMessage().split(" ");
+                    if(10 <= PermUtils.getPermLevel(event.getBot(), event.getUser(), event.getChannel())){
+
 			 if (messageParts[0].equalsIgnoreCase((GeneralRegistry.CommandChar + "kick"))){
 	            	event.getChannel().send().kick(IRCUtils.getUserByNick(event.getChannel(), messageParts[1]));
 			 } else 
                              if (messageParts[0].equalsIgnoreCase((GeneralRegistry.CommandChar + "mode"))){
 	            	event.getChannel().send().setMode(messageParts[1]);
-			 } else if (messageParts[0].equalsIgnoreCase((GeneralRegistry.CommandChar + "somethingawesome"))){
-                            if(event.getChannel().getUserLevels(event.getBot().getUserBot()).contains(UserLevel.OP) && event.getChannel().isOwner(event.getUser()) == false && event.getChannel().isSuperOp(event.getUser()) == false){
-                             event.getChannel().send().kick(event.getUser(),"http://bit.ly/1c9vo1S");
-                            } else if(event.getChannel().getUserLevels(event.getBot().getUserBot()).contains(UserLevel.OWNER)){
-                             event.getChannel().send().kick(event.getUser(),"http://bit.ly/1c9vo1S");
-                            }else {
-                                event.getChannel().send().message("http://bit.ly/1c9vo1S");
-
-                            }
-			 }
+			 } 
+			 }else {
+                     event.getChannel().send().message("Permission Denied");
+                 }
                          
                          
 	} 
-}
+                }
+
 	
--- src/com/techcavern/wavetact/commands/BasicCommands.java
+++ src/com/techcavern/wavetact/commands/BasicCommands.java
@@ -4,14 +4,33 @@ import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
 
 import com.techcavern.wavetact.utils.*;
+import org.pircbotx.UserLevel;
 public class BasicCommands extends ListenerAdapter<PircBotX> {
 	public void onMessage(MessageEvent<PircBotX> event) throws Exception{
 	   IRCUtils.onMessage("ping","pong",event.getChannel(),event.getUser(),event.getBot(),event.getMessage(),0);     
 	   IRCUtils.onMessage("pong","ping",event.getChannel(),event.getUser(),event.getBot(),event.getMessage(),0);
 	   IRCUtils.onMessage("Source","https://github.com/TechCavern/WaveTact",event.getChannel(),event.getUser(),event.getBot(),event.getMessage(),0);
+            if (event.getMessage().equalsIgnoreCase((GeneralRegistry.CommandChar + "somethingawesome"))){
+                            if(event.getChannel().getUserLevels(event.getBot().getUserBot()).contains(UserLevel.OP) && event.getChannel().isOwner(event.getUser()) == false && event.getChannel().isSuperOp(event.getUser()) == false){
+                             event.getChannel().send().kick(event.getUser(),"http://bit.ly/1c9vo1S");
+                            } else if(event.getChannel().getUserLevels(event.getBot().getUserBot()).contains(UserLevel.OWNER)){
+                             event.getChannel().send().kick(event.getUser(),"http://bit.ly/1c9vo1S");
+                            }else {
+                                event.getChannel().send().message("http://bit.ly/1c9vo1S");
 
-	}
-}            
+                            }
+            }
+            if (5 <= PermUtils.getPermLevel(event.getBot(), event.getUser(), event.getChannel())){
+                if(event.getMessage().toLowerCase().startsWith(GeneralRegistry.CommandChar+"say")){
+                    event.getChannel().send().message(event.getMessage().replace(GeneralRegistry.CommandChar+"say ",""));
+                    
+                } else if(event.getMessage().toLowerCase().startsWith(GeneralRegistry.CommandChar+"act")){
+                    event.getChannel().send().action(event.getMessage().replace(GeneralRegistry.CommandChar+"act ",""));
+                    
+                }
+            }
+}  
+}
 
 	        
 
