aacf41ce7b609874b68d6ab43beaa41c5c39711d
julian zhou <jzhou2oo6@me.com>
2014-06-22 10:27:14 -0400

    Move Trusted Commands to their own package

--- src/com/techcavern/wavetact/commands/trusted/Act.java
+++ src/com/techcavern/wavetact/commands/trusted/Act.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.techcavern.wavetact.commands;
+package com.techcavern.wavetact.commands.trusted;
 
 import com.techcavern.wavetact.objects.Command;
 import com.techcavern.wavetact.utils.GeneralRegistry;
@@ -12,15 +12,15 @@ import org.pircbotx.hooks.events.MessageEvent;
 /**
  * @author jztech101
  */
-public class Say extends Command {
+public class Act extends Command {
 
-    public Say() {
-        super("say", 5);
+    public Act() {
+        super("act", 5);
     }
 
     @Override
     public void onCommand(MessageEvent<?> event, String... args) throws Exception {
-        event.getChannel().send().message(event.getMessage().replace(GeneralRegistry.CommandChar + "say ", ""));
+        event.getChannel().send().action(event.getMessage().replace(GeneralRegistry.CommandChar + "act ", ""));
 
     }
 }
