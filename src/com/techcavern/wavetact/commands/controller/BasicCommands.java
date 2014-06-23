8a16cbcf99dbf9bf6035d99c6325f34785bfdcd9
julian zhou <jzhou2oo6@me.com>
2014-06-23 16:43:42 -0400

    made BasicCommands Controller only

--- src/com/techcavern/wavetact/commands/controller/BasicCommands.java
+++ src/com/techcavern/wavetact/commands/controller/BasicCommands.java
@@ -10,7 +10,7 @@ import org.pircbotx.hooks.events.MessageEvent;
 
 public class BasicCommands extends Command {
     public BasicCommands() {
-        super("AddBasicCommands", 10);
+        super("AddBasicCommands", 9001);
     }
 
     @Override
