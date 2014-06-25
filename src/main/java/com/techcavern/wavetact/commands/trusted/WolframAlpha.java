b733b54465a54862b181cda913b2e1a31f71d4c6
julian zhou <jzhou2oo6@me.com>
2014-06-25 16:33:46 -0400

    Fix Imports

--- src/main/java/com/techcavern/wavetact/commands/trusted/WolframAlpha.java
+++ src/main/java/com/techcavern/wavetact/commands/trusted/WolframAlpha.java
@@ -2,30 +2,27 @@ package com.techcavern.wavetact.commands.trusted;
 
 import com.techcavern.wavetact.objects.Command;
 import com.techcavern.wavetact.utils.GeneralUtils;
-import com.wolfram.alpha.WAPlainText;
-import com.wolfram.alpha.WAPod;
-import com.wolfram.alpha.WAQueryResult;
-import com.wolfram.alpha.WASubpod;
 import org.apache.commons.lang3.StringUtils;
 import org.pircbotx.hooks.events.MessageEvent;
-
-import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by jztech101 on 6/23/14.
- */
+
 public class WolframAlpha extends Command {
     public WolframAlpha() {
-        super("wa", 5, "wa [input]");
+        super("wa", 5, "wa [input] (return array value #)");
     }
 
     @Override
     public void onCommand(MessageEvent<?> event, String... args) throws Exception {
         List<String> waresults = GeneralUtils.getWAResult(StringUtils.join(args," "));
+
+        if (args.length > 0) {
+            event.respond(waresults.get(Integer.parseInt(args[1])));
+        } else {
         for(String results: waresults){
            event.respond(results);
 
+        }
         }
         }
 }
