993b669941b4f7103296937bb51babe10fb11400
julian zhou <jzhou2oo6@me.com>
2014-07-03 13:40:49 -0400

    Added Half Op Command and added alias for protect to sop and voice to vop

--- src/main/java/com/techcavern/wavetact/commands/chanop/HalfOp.java
+++ src/main/java/com/techcavern/wavetact/commands/chanop/HalfOp.java
@@ -11,29 +11,28 @@ import com.techcavern.wavetact.utils.GeneralUtils;
 import com.techcavern.wavetact.utils.IRCUtils;
 import org.pircbotx.hooks.events.MessageEvent;
 
-
 /**
  * @author jztech101
  */
-public class Voice extends Command {
+public class HalfOp extends Command {
     @CMD
-    public Voice() {
-        super(GeneralUtils.toArray("voice vo vop"), 9, "Voice (-)(User)");
+    public HalfOp() {
+        super(GeneralUtils.toArray("halfop hop"), 9, "halfop (-)(user to  halfop)");
     }
 
     @Override
     public void onCommand(MessageEvent<?> event, String... args) throws Exception {
         if (args[0] != null) {
             if (args[0].equalsIgnoreCase("-")) {
-                event.getChannel().send().deVoice(event.getUser());
+                event.getChannel().send().deHalfOp(event.getUser());
             } else if (args[0].startsWith("-")) {
-                event.getChannel().send().deVoice(IRCUtils.getUserByNick(event.getChannel(), args[0].replaceFirst("-", "")));
+                event.getChannel().send().deHalfOp(IRCUtils.getUserByNick(event.getChannel(), args[0].replaceFirst("-", "")));
             } else {
-                event.getChannel().send().voice(IRCUtils.getUserByNick(event.getChannel(), args[0]));
+                event.getChannel().send().halfOp(IRCUtils.getUserByNick(event.getChannel(), args[0]));
 
             }
         } else {
-            event.getChannel().send().voice(event.getUser());
+            event.getChannel().send().halfOp(event.getUser());
         }
     }
 }
