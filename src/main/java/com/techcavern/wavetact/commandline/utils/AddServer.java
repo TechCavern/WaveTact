4764315e4f91a4cf842b1ceeb912b691e2f05822
julian zhou <jzhou2oo6@me.com>
2014-07-06 20:15:20 -0400

    update server command to reflect that "False" disables it

--- src/main/java/com/techcavern/wavetact/commandline/utils/AddServer.java
+++ src/main/java/com/techcavern/wavetact/commandline/utils/AddServer.java
@@ -30,7 +30,7 @@ public class AddServer extends CommandLine {
             System.out.print("Channels(#chan1, #chan2): ");
             config.set("channels", input.nextLine());
             System.out.print("Nickserv Pass(empty for none): ");
-            config.set("nickserv", input.nextLine());
+            config.set("nickserv (type False to disable)", input.nextLine());
             System.out.print("Command Prefix: ");
             config.set("prefix", input.nextLine());
             config.save();
