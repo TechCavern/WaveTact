9c3cfd5d866467b5801240bd2b1badc11a8e22fb
julian zhou <jzhou2oo6@me.com>
2014-08-03 16:22:04 -0400

    Wrong StringUtils

--- src/main/java/com/techcavern/wavetact/commands/controller/Globals.java
+++ src/main/java/com/techcavern/wavetact/commands/controller/Globals.java
@@ -5,7 +5,7 @@
  */
 package com.techcavern.wavetact.commands.controller;
 
-import com.sun.deploy.util.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import com.techcavern.wavetact.annot.CMD;
 import com.techcavern.wavetact.annot.ConCMD;
 import com.techcavern.wavetact.utils.*;
