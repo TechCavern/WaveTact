bb4819b61401109b0cf8d1d5c16e77c43e6c9836
julian zhou <jzhou2oo6@me.com>
2014-11-05 18:37:07 -0500

    Finish Pokemon Package (Fixes #41)

--- src/main/java/com/techcavern/wavetact/commands/pokemon/Pokeability.java
+++ src/main/java/com/techcavern/wavetact/commands/pokemon/Pokeability.java
@@ -1,35 +1,30 @@
 package com.techcavern.wavetact.commands.pokemon;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.techcavern.wavetact.annot.CMD;
 import com.techcavern.wavetact.annot.GenCMD;
 import com.techcavern.wavetact.utils.GeneralUtils;
 import com.techcavern.wavetact.utils.IRCUtils;
 import com.techcavern.wavetact.utils.objects.GenericCommand;
-import org.apache.commons.lang3.StringUtils;
 import org.pircbotx.Channel;
 import org.pircbotx.PircBotX;
 import org.pircbotx.User;
 
-@CMD
-@GenCMD
-public class Pokemove extends GenericCommand {
+//@CMD
+//@GenCMD
+//Deregistered due to lack of usefulness with missing descriptions and all
+public class Pokeability extends GenericCommand {
 
-    public Pokemove() {
-        super(GeneralUtils.toArray("pokemove pkmo"), 0, "pokemove [ID]", "Displays info on a move");
+    public Pokeability() {
+        super(GeneralUtils.toArray("pokeability pka"), 0, "pokeability [ID]", "Displays info on an ability");
     }
 
     @Override
     public void onCommand(User user, PircBotX Bot, Channel channel, boolean isPrivate, int UserPermLevel, String... args) throws Exception {
-        JsonObject pokemon = GeneralUtils.getJsonObject("http://pokeapi.co/api/v1/move/" + args[0]);
+        JsonObject pokemon = GeneralUtils.getJsonObject("http://pokeapi.co/api/v1/ability/" + args[0]);
         String name = pokemon.get("name").getAsString();
         String id = pokemon.get("id").getAsString();
         String description = pokemon.get("description").getAsString().replace("\n", " ");
-        String accuracy = pokemon.get("accuracy").getAsString();
-        String power = pokemon.get("power").getAsString();
-        String pp = pokemon.get("pp").getAsString();
         IRCUtils.sendMessage(user,channel,name + "(" + id + "): " + description ,isPrivate);
-        IRCUtils.sendMessage(user, channel, "Accuracy: " + accuracy + " - Power: " + power +" - PP: " + pp, isPrivate);
     }
 }
