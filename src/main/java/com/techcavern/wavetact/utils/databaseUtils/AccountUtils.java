e7f4e439fdd6855ec6e8aad95e6cb0a2a16f3455
jztech101 <jzhou2oo6@gmail.com>
2014-12-30 18:02:52 -0500

    Ok, Got one part working

--- src/main/java/com/techcavern/wavetact/utils/databaseUtils/AccountUtils.java
+++ src/main/java/com/techcavern/wavetact/utils/databaseUtils/AccountUtils.java
@@ -1,18 +1,29 @@
 package com.techcavern.wavetact.utils.databaseUtils;
 
 import com.techcavern.wavetact.utils.Registry;
+import org.jooq.Record;
+import org.jooq.Result;
+
 import static com.techcavern.wavetactdb.Tables.*;
 
 public class AccountUtils {
     @SuppressWarnings("unchecked")
-    public static void getA() {
-    }
 
-    public static void saveAccounts() {
-
-    }
 
     public static String getAccountPassword(String Account) {
-        return Registry.WaveTactDB.select().from(ACCOUNTS).where(ACCOUNTS.USERNAME.eq(Account)).fetch().getValue(ACCOUNTS.PASSWORD);
+        Result<Record> accountrecord =  Registry.WaveTactDB.select().from(ACCOUNTS).where(ACCOUNTS.USERNAME.eq(Account)).fetch();
+        if(accountrecord.size() > 0){
+            return accountrecord.get(0).getValue(ACCOUNTS.PASSWORD);
+        }else{
+            return null;
+        }
+    }
+    public static String setAccountPassword(String Account, String password) {
+        Result<Record> accountrecord =  Registry.WaveTactDB.select().from(ACCOUNTS).where(ACCOUNTS.USERNAME.eq(Account)).fetch();
+        if(accountrecord.size() > 0){
+            return accountrecord.get(0).setValue(ACCOUNTS.PASSWORD, password);
+        }else{
+            return null;
+        }
     }
 }
