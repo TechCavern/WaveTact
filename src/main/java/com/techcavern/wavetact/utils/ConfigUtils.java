7d4c031fcafc76f95a38346dd6959dd19989019f
James Lu <GLolol1@hotmail.com>
2015-06-21 11:26:59 -0700

    where's my money

--- src/main/java/com/techcavern/wavetact/utils/ConfigUtils.java
+++ src/main/java/com/techcavern/wavetact/utils/ConfigUtils.java
@@ -82,7 +82,7 @@ public class ConfigUtils {
         Net.setAutoReconnectDelay(20000);
         Net.setChannelPrefixes("#");
         Net.setUserLevelPrefixes("+%@&~!");
-        Net.setVersion("WaveTact 0.8.0");
+        Net.setVersion("WaveTact 0.8.0-dev");
         Net.setAutoReconnect(true);
         if (nickservPassword != null) {
             Net.setNickservPassword(nickservPassword);
