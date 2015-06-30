1a302297927786471b2f4ab616bec661db4611a1
jztech101 <jzhou2oo6@gmail.com>
2015-06-30 18:28:38 -0400

    Fixing some derps

--- src/main/java/com/techcavern/wavetact/eventListeners/FunMsgListener.java
+++ src/main/java/com/techcavern/wavetact/eventListeners/FunMsgListener.java
@@ -29,22 +29,7 @@ public class FunMsgListener extends ListenerAdapter {
     public void onMessage(MessageEvent event) throws Exception {
         class process implements Runnable {
             public void run() {
-                if (PermUtils.getPermLevel(event.getBot(), event.getUser().getNick(), event.getChannel()) > -2) {
-                        if (Colors.removeFormattingAndColors(event.getMessage()).contains("stop-irc-bullying.eu")) {
-                            if (IRCUtils.checkIfCanKick(event.getChannel(), event.getBot(), event.getUser())) {
-                                IRCUtils.sendKick(event.getBot().getUserBot(), event.getUser(), event.getBot(), event.getChannel(), "┻━┻ ︵ ¯\\ (ツ)/¯ ︵ ┻━┻ [https://goo.gl/Tkb9dh]");
-                            } else {
-                                IRCUtils.sendAction(event.getUser(), event.getBot(), event.getChannel(), "kicks " + event.getUser().getNick() + " (┻━┻ ︵ ¯\\ (ツ)/¯ ︵ ┻━┻) [https://goo.gl/Tkb9dh]", "");
-                            }
-                        }
-                    /**
-                     * My apologies to those using this site responsibly. But in my experience, this site has been linked numerous times for entertainment purposes
-                     * In fact, I have yet to notice a time when it is linked for its intended purpose. And if you are using this site for its intended purpose, please think of
-                     * better of way of expressing how you feel. Linking a generic site rarely solves any problems. Instead explain to the person how and why they offended you. If
-                     * they ignore you, then you ignore them.
-                     */
-                    Record autourl = DatabaseUtils.getChannelProperty(IRCUtils.getNetworkNameByNetwork(event.getBot()), event.getChannel().getName(), "autourl");
-                    if (autourl != null && autourl.getValue(CHANNELPROPERTY.VALUE).equalsIgnoreCase("true") && !event.getMessage().startsWith(DatabaseUtils.getConfig("commandchar"))) {
+                if (PermUtils.getPermLevel(event.getBot(), event.getUser().getNick(), event.getChannel()) > -2 && !event.getMessage().startsWith(DatabaseUtils.getConfig("commandchar"))) {
                         String[] message = StringUtils.split(event.getMessage(), " ");
                         for (String arg : message) {
                             try {
@@ -52,11 +37,28 @@ public class FunMsgListener extends ListenerAdapter {
                                 if (!arg.startsWith("https://") && !arg.startsWith("http://")) {
                                     arg = "http://" + arg;
                                 }
-                                if (Registry.urlvalidator.isValid(arg) && !arg.contains("stop-irc-bullying.eu")) {
+                                if (Registry.urlvalidator.isValid(arg)) {
                                     Document doc = Jsoup.connect(arg).userAgent("Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17").get();
-                                    String title = doc.title();
-                                    if (!title.isEmpty()) {
-                                        IRCUtils.sendMessage(event.getBot(), event.getChannel(), "[" + event.getUser().getNick() + "] - " + title, "");
+                                    if(doc.location().contains("stop-irc-bullying.eu")) {
+                                        if (IRCUtils.checkIfCanKick(event.getChannel(), event.getBot(), event.getUser())) {
+                                            IRCUtils.sendKick(event.getBot().getUserBot(), event.getUser(), event.getBot(), event.getChannel(), "┻━┻ ︵ ¯\\ (ツ)/¯ ︵ ┻━┻ [https://goo.gl/Tkb9dh]");
+                                        } else {
+                                            IRCUtils.sendAction(event.getUser(), event.getBot(), event.getChannel(), "kicks " + event.getUser().getNick() + " (┻━┻ ︵ ¯\\ (ツ)/¯ ︵ ┻━┻) [https://goo.gl/Tkb9dh]", "");
+                                        }
+                                        /**
+                                         * My apologies to those using this site responsibly. But in my experience, this site has been linked numerous times for entertainment purposes
+                                         * In fact, I have yet to notice a time when it is linked for its intended purpose. And if you are using this site for its intended purpose, please think of
+                                         * better of way of expressing how you feel. Linking a generic site rarely solves any problems. Instead explain to the person how and why they offended you. If
+                                         * they ignore you, then you ignore them.
+                                         */
+                                    }else {
+                                        Record autourl = DatabaseUtils.getChannelProperty(IRCUtils.getNetworkNameByNetwork(event.getBot()), event.getChannel().getName(), "autourl");
+                                        if (autourl != null && autourl.getValue(CHANNELPROPERTY.VALUE).equalsIgnoreCase("true")) {
+                                            String title = doc.title();
+                                            if (!title.isEmpty()) {
+                                                IRCUtils.sendMessage(event.getBot(), event.getChannel(), "[" + event.getUser().getNick() + "] - " + title, "");
+                                            }
+                                        }
                                     }
                                 }
                             } catch (Exception e) {
@@ -66,7 +68,6 @@ public class FunMsgListener extends ListenerAdapter {
                     }
                 }
             }
-        }
         Registry.threadPool.execute(new process());
     }
 }
