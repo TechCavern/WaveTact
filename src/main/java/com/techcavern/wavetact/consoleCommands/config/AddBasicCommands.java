3a521b3978a98528f17514944054156d695aaf21
jztech101 <jzhou2oo6@gmail.com>
2015-07-01 21:36:09 -0400

    Remove Useless Basic Commands (Resolves #137)

--- src/main/java/com/techcavern/wavetact/consoleCommands/config/AddBasicCommands.java
+++ src/main/java/com/techcavern/wavetact/consoleCommands/config/AddBasicCommands.java
@@ -20,11 +20,8 @@ public class AddBasicCommands extends ConsoleCommand {
         DatabaseUtils.addCustomCommand(null, null, "ping", 0, "pong", true, false);
         DatabaseUtils.addCustomCommand(null, null, "pong", 0, "ping", true, false);
         DatabaseUtils.addCustomCommand(null, null, "releases", 0, "https://github.com/TechCavern/WaveTact/releases", true, false);
-        DatabaseUtils.addCustomCommand(null, null, "license", 0, "MIT (Do What The Fuck You Want, We take ZERO liability)", true, false);
+        DatabaseUtils.addCustomCommand(null, null, "license", 0, "MIT License (https://github.com/TechCavern/WaveTact/blob/master/license.txt)", true, false);
         DatabaseUtils.addCustomCommand(null, null, "source", 0, "http://github.com/TechCavern/WaveTact", true, false);
-        DatabaseUtils.addCustomCommand(null, null, "authors", 0, "Julian (JZTech101)", true, false);
-        DatabaseUtils.addCustomCommand(null, null, "contributors", 0, "https://github.com/TechCavern/WaveTact/graphs/contributors", true, false);
-        DatabaseUtils.addCustomCommand(null, null, "nom", 0, "noms on $*", true, true);
 
         commandIO.getPrintStream().println("Basic Commands Added");
     }
