0d4b479d60a6ff027222f7da8e3f2b872e022811
jztech101 <jzhou2oo6@gmail.com>
2015-07-05 20:14:48 -0400

    No more pings from admin command

--- src/main/java/com/techcavern/wavetact/ircCommands/utils/Admins.java
+++ src/main/java/com/techcavern/wavetact/ircCommands/utils/Admins.java
@@ -3,9 +3,13 @@ package com.techcavern.wavetact.ircCommands.utils;
 import com.techcavern.wavetact.annot.IRCCMD;
 import com.techcavern.wavetact.objects.IRCCommand;
 import com.techcavern.wavetact.utils.*;
+import org.apache.commons.lang3.StringUtils;
 import org.pircbotx.Channel;
 import org.pircbotx.PircBotX;
 import org.pircbotx.User;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static com.techcavern.wavetactdb.Tables.SERVERS;
 
@@ -18,6 +22,10 @@ public class Admins extends IRCCommand {
 
     @Override
     public void onCommand(String command, User user, PircBotX network, String prefix, Channel channel, boolean isPrivate, int userPermLevel, String... args) throws Exception {
-        IRCUtils.sendMessage(user, network, channel,DatabaseUtils.getServer(IRCUtils.getNetworkNameByNetwork(network)).getValue(SERVERS.NETWORKADMINS), prefix);
+        String[] admins = StringUtils.split(DatabaseUtils.getServer(IRCUtils.getNetworkNameByNetwork(network)).getValue(SERVERS.NETWORKADMINS), ",");
+        for (int i = 0; i < admins.length; i++) {
+            admins[i] = GeneralUtils.replaceVowelsWithAccents(admins[i].replace(" ", ""));
+        }
+        IRCUtils.sendMessage(user, network, channel, StringUtils.join(admins, ", "), prefix);
     }
 }
