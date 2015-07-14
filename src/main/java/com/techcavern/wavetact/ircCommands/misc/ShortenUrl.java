b5b88d098ed15c1dc9ae44269a39dd736b941e80
jztech101 <jzhou2oo6@gmail.com>
2015-07-14 13:58:06 -0400

    Random Commit

---
+++ src/main/java/com/techcavern/wavetact/ircCommands/misc/ShortenUrl.java
@@ -0,0 +1,31 @@
+package com.techcavern.wavetact.ircCommands.misc;
+
+import com.google.gson.JsonObject;
+import com.techcavern.wavetact.annot.IRCCMD;
+import com.techcavern.wavetact.objects.IRCCommand;
+import com.techcavern.wavetact.utils.ErrorUtils;
+import com.techcavern.wavetact.utils.GeneralUtils;
+import com.techcavern.wavetact.utils.IRCUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.pircbotx.Channel;
+import org.pircbotx.PircBotX;
+import org.pircbotx.User;
+
+@IRCCMD
+public class ShortenUrl extends IRCCommand {
+
+    public ShortenUrl() {
+        super(GeneralUtils.toArray("shortenUrl sUrl"), 0, "shortenUrl [url]", "Shortens Url", false);
+    }
+
+    @Override
+    public void onCommand(String command, User user, PircBotX network, String prefix, Channel channel, boolean isPrivate, int userPermLevel, String... args) throws Exception {
+        String shortUrl = GeneralUtils.shortenURL(args[0]);
+        if(shortUrl.isEmpty()){
+            ErrorUtils.sendError(user, "Bit.ly OAuth Token is not defined. Contact Bot Controller to fix.");
+            return;
+        }
+        IRCUtils.sendMessage(user, network, channel, shortUrl, prefix);
+    }
+}
+
