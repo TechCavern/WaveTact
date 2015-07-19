461a17c75fd7653bb311e006850353f5df611377
jztech101 <jzhou2oo6@gmail.com>
2015-07-19 14:00:13 -0400

    Reformat MC Status

--- src/main/java/com/techcavern/wavetact/ircCommands/minecraft/MCStatus.java
+++ src/main/java/com/techcavern/wavetact/ircCommands/minecraft/MCStatus.java
@@ -1,17 +1,18 @@
 package com.techcavern.wavetact.ircCommands.minecraft;
 
+import com.google.gson.JsonArray;
 import com.techcavern.wavetact.annot.IRCCMD;
 import com.techcavern.wavetact.objects.IRCCommand;
 import com.techcavern.wavetact.utils.GeneralUtils;
 import com.techcavern.wavetact.utils.IRCUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.text.WordUtils;
 import org.pircbotx.Channel;
 import org.pircbotx.PircBotX;
 import org.pircbotx.User;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 @IRCCMD
 public class MCStatus extends IRCCommand {
@@ -22,13 +23,33 @@ public class MCStatus extends IRCCommand {
 
     @Override
     public void onCommand(String command, User user, PircBotX network, String prefix, Channel channel, boolean isPrivate, int userPermLevel, String... args) throws Exception {
-        String result;
-        URL url = new URL("https://status.mojang.com/check");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-        result = reader.readLine().replace("\"},{\"", " - ").replace("\":\"", ": ").replace("green", "Online").replace("red", "Offline").replace("[{\"", "").replace("\"}]", "").replace(".minecraft.net", "").replace(".mojang.com", "").replace("server", " Server").replace(".net", "");
-        result = WordUtils.capitalizeFully(result);
+        List<String> result = new ArrayList<>();
+        JsonArray mcstatus = GeneralUtils.getJsonArray("https://status.mojang.com/check");
+        mcstatus.forEach(status -> {
+            String name = status.getAsJsonObject().entrySet().iterator().next().getKey().toString();
+            if (name.equalsIgnoreCase("minecraft.net")) {
+                name = "Website";
+            } else if (name.equalsIgnoreCase("api.mojang.com")) {
+                name = "API";
+            } else if (name.equalsIgnoreCase("authserver.mojang.com")) {
+                name = "AuthServer";
+            } else if (name.equalsIgnoreCase("sessionserver.mojang.com")) {
+                name = "SessionServer";
+            } else {
+                name = WordUtils.capitalize(name.replace(".minecraft.net", "").replace(".mojang.com", ""));
+            }
+            String value = status.getAsJsonObject().entrySet().iterator().next().getValue().getAsString();
+            if (value.equalsIgnoreCase("green")) {
+                value = "Online";
+            } else if (value.equalsIgnoreCase("yellow")) {
+                value = "Slow";
+            } else {
+                value = "Offline";
+            }
+            result.add(name + ": " + value);
+        });
         if (result != null) {
-            IRCUtils.sendMessage(user, network, channel, result, prefix);
+            IRCUtils.sendMessage(user, network, channel, StringUtils.join(result, " - "), prefix);
         } else {
             IRCUtils.sendError(user, network, channel, "MC status currently unavailable", prefix);
         }
