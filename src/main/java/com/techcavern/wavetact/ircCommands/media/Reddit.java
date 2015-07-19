e0ab97075bffdc14e5d7d51ff98e9aa9030a7cd2
jztech101 <jzhou2oo6@gmail.com>
2015-07-19 10:09:53 -0400

    Quarter the time for reddit to retrieve the data

--- src/main/java/com/techcavern/wavetact/ircCommands/media/Reddit.java
+++ src/main/java/com/techcavern/wavetact/ircCommands/media/Reddit.java
@@ -21,7 +21,7 @@ public class Reddit extends IRCCommand {
 
     @Override
     public void onCommand(String command, User user, PircBotX network, String prefix, Channel channel, boolean isPrivate, int userPermLevel, String... args) throws Exception {
-        JsonArray results = GeneralUtils.getJsonObject("http://api.reddit.com/r/" + args[0] + "/?limit=100").get("data").getAsJsonObject().get("children").getAsJsonArray();
+        JsonArray results = GeneralUtils.getJsonObject("http://api.reddit.com/r/" + args[0] + "/?limit=25").get("data").getAsJsonObject().get("children").getAsJsonArray();
         JsonObject result = results.get(new Random().nextInt(results.size() - 1)).getAsJsonObject().get("data").getAsJsonObject();
         IRCUtils.sendMessage(user, network, channel, result.get("title").getAsString() + " by " + result.get("author").getAsString() + " - " + GeneralUtils.shortenURL(result.get("url").getAsString()), prefix);
     }
