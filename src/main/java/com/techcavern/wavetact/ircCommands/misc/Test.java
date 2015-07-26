1b509292792c1eff31ac0b602d0ad830ea4dc96b
Julian Zhou <jzhou2oo6@gmail.com>
2015-07-26 16:50:13 -0400

    Clear test code

--- src/main/java/com/techcavern/wavetact/ircCommands/misc/Test.java
+++ src/main/java/com/techcavern/wavetact/ircCommands/misc/Test.java
@@ -18,31 +18,6 @@ public class Test extends IRCCommand {
 
     @Override
     public void onCommand(String command, User user, PircBotX network, String prefix, Channel channel, boolean isPrivate, int userPermLevel, String... args) throws Exception {
-        JsonArray versions = GeneralUtils.getJsonArray("http://bot.notenoughmods.com/?json&count");
-        MCModVersionSearch:
-        for (int i = versions.size() - 1; i >= 0; i--) {
-            int size = versions.get(i).getAsJsonObject().get("count").getAsInt();
-            if (size > 10) {
-                String version = versions.get(i).getAsJsonObject().get("name").getAsString();
-                JsonArray mods = GeneralUtils.getJsonArray("http://bot.notenoughmods.com/" + version + ".json");
-                JsonArray mods2 = GeneralUtils.getJsonArray("http://modlist.mcf.li/api/v3/" + version + ".json");
-                mods2.forEach(mod2 -> {
-                    JsonObject mod = mod2.getAsJsonObject();
-                    String searchPhrase = mod.get("name").getAsString().replaceAll(" ", "").replaceAll("[^a-zA-Z0-9]", "");
-                    boolean isTrue = false;
-                    int j = 0;
-                    while (j < mods.size() && !isTrue) {
-                        JsonObject mod24 = mods.get(j).getAsJsonObject();
-                        if (mod24.get("name").getAsString().replaceAll(" ", "").replaceAll("[^a-zA-Z0-9]", "").equalsIgnoreCase(searchPhrase)) {
-                            isTrue = true;
-                        }
-                        j++;
-                    }
-                    if (!isTrue)
-                        IRCUtils.sendMessage(user, network, channel, "[" + version + "] " + searchPhrase, prefix);
-                });
-            }
-
-        }
+       
     }
 }
