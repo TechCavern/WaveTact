74f76c4a6f87fd95e2b45ef1aaed9895c241dad3
Julian Zhou <jzhou2oo6@gmail.com>
2016-01-21 14:58:40 -0500

    Add TLDInfo Command

--- src/main/java/com/techcavern/wavetact/ircCommands/dnsinfo/TLDInfo.java
+++ src/main/java/com/techcavern/wavetact/ircCommands/dnsinfo/TLDInfo.java
@@ -8,6 +8,8 @@ import com.techcavern.wavetact.utils.Registry;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.pircbotx.Channel;
 import org.pircbotx.PircBotX;
 import org.pircbotx.User;
@@ -26,8 +28,14 @@ public class TLDInfo extends IRCCommand {
         }
         try {
             Document doc = Jsoup.connect("http://www.iana.org/domains/root/db/" + args[0]).userAgent(Registry.USER_AGENT).get();
-            String c = doc.select("#main_right").text();
-            IRCUtils.sendMessage(user, network, channel, c, prefix);
+            Elements titles = doc.select("#main_right").select("h2");
+            Elements names = doc.select("#main_right").select("b");
+            String[] organization = doc.select("#main_right").after("br").html().split("\n");
+            String sponsor = "Sponsoring Organization: "+ names.get(0).text();
+            String admin =  "Administrative Contact: " + names.get(1).text()  + ", " +  GeneralUtils.stripHTML(organization[10]);
+            String tech = "Technical Contact: " + names.get(5).text() + ", " + GeneralUtils.stripHTML(organization[23]);
+            IRCUtils.sendMessage(user, network, channel, sponsor + " - " + admin + " - " + tech , prefix);
+
         }catch(HttpStatusException e){
             if(e.getStatusCode() == 404){
                 IRCUtils.sendError(user, network, channel, "Invalid TLD", prefix);
