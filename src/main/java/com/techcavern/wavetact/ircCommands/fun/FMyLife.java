a4a03a0c13222f166498be3dc88b786a94d866e3
Julian Zhou <jzhou2oo6@gmail.com>
2016-04-27 18:35:26 -0400

    Fix FML

--- src/main/java/com/techcavern/wavetact/ircCommands/fun/FMyLife.java
+++ src/main/java/com/techcavern/wavetact/ircCommands/fun/FMyLife.java
@@ -22,8 +22,8 @@ public class FMyLife extends IRCCommand {
     @Override
     public void onCommand(String command, User user, PircBotX network, String prefix, Channel channel, boolean isPrivate, int userPermLevel, String... args) throws Exception {
         Document doc = Jsoup.connect("http://m.fmylife.com/random").userAgent(Registry.USER_AGENT).get();
-        Elements FML = doc.select(".text");
-        String fmylife = GeneralUtils.stripHTML(FML.get(0).toString());
+        Elements FML = doc.select(".post");
+        String fmylife = GeneralUtils.stripHTML(FML.get(0).select(".content").toString());
         IRCUtils.sendMessage(user, network, channel, fmylife, prefix);
     }
 }
