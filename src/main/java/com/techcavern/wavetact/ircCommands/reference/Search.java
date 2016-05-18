5bfe33024407ceb53b660dbe9ee0fc8730a81b11
Julian Zhou <jzhou2oo6@gmail.com>
2016-05-18 18:07:39 -0400

    Switch from Google to DogPile (Resolves #213)

--- src/main/java/com/techcavern/wavetact/ircCommands/reference/Search.java
+++ src/main/java/com/techcavern/wavetact/ircCommands/reference/Search.java
@@ -7,6 +7,9 @@ import com.techcavern.wavetact.utils.GeneralUtils;
 import com.techcavern.wavetact.utils.IRCUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.pircbotx.Channel;
 import org.pircbotx.PircBotX;
 import org.pircbotx.User;
@@ -15,7 +18,7 @@ import org.pircbotx.User;
 public class Search extends IRCCommand {
 
     public Search() {
-        super(GeneralUtils.toArray("search google g goog gsearch"), 1, "search (result #) [string to google]", "Googles something", false);
+        super(GeneralUtils.toArray("search google g dogpile yahoo bing"), 1, "search (result #) [string to google]", "Googles something", false);
     }
 
     @Override
@@ -25,12 +28,13 @@ public class Search extends IRCCommand {
             ArrayIndex = Integer.parseInt(args[0]) - 1;
             args = ArrayUtils.remove(args, 0);
         }
-        JsonArray results = GeneralUtils.getJsonObject("https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" + StringUtils.join(args, "%20")).getAsJsonObject("responseData").getAsJsonArray("results");
+        Document doc = Jsoup.connect("http://www.dogpile.com/dogpilecontrol/search/web?fcoid=417&fcop=topnav&fpid=27&q=thing" + StringUtils.join(args, "%20")).get();
+        Elements results = doc.select(".searchResult");
         if (results.size() > 0) {
             if (results.size() - 1 >= ArrayIndex) {
-                String title = results.get(ArrayIndex).getAsJsonObject().get("titleNoFormatting").getAsString();
-                String content = GeneralUtils.stripHTML(results.get(ArrayIndex).getAsJsonObject().get("content").getAsString());
-                String url = results.get(ArrayIndex).getAsJsonObject().get("unescapedUrl").getAsString();
+                String title = results.get(ArrayIndex).select(".resultTitlePane").text();
+                String url = results.get(ArrayIndex).select(".resultDisplayUrl").text();
+                String content = results.get(ArrayIndex).select(".resultDescription").text();
                 IRCUtils.sendMessage(user, network, channel, "[" + title + "] " + content + " - " + GeneralUtils.shortenURL(url), prefix);
             } else {
                 ArrayIndex = ArrayIndex + 1;
