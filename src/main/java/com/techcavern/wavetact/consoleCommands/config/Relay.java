45f2ea81e0ffe809c14f093296962ac35ed59c99
Julian Zhou <jzhou2oo6@gmail.com>
2018-01-02 03:37:20 -0600

    Resolves #243

--- src/main/java/com/techcavern/wavetact/consoleCommands/config/Relay.java
+++ src/main/java/com/techcavern/wavetact/consoleCommands/config/Relay.java
@@ -38,21 +38,21 @@ public class Relay extends ConsoleCommand {
         if (relay != null && (isDelete || isModify)) {
             if (isDelete) {
                 DatabaseUtils.removeRelay(args[0]);
-                commandIO.getPrintStream().println("Property removed");
+                commandIO.getPrintStream().println("Relay removed");
             } else if (isModify) {
                 if (viewonly)
                     commandIO.getPrintStream().println(relay.getValue(RELAYS.VALUE));
                 else {
                     relay.setValue(RELAYS.VALUE, GeneralUtils.buildMessage(1, args.length, args));
                     DatabaseUtils.updateRelay(relay);
-                    commandIO.getPrintStream().println("Property modified");
+                    commandIO.getPrintStream().println("Relay modified");
                 }
             }
         } else if (relay == null && !isDelete && !isModify) {
             DatabaseUtils.addRelay(args[0], GeneralUtils.buildMessage(1, args.length, args));
-            commandIO.getPrintStream().println("Property added");
+            commandIO.getPrintStream().println("Relay added");
         } else {
-            commandIO.getPrintStream().println("property already exists (If you were adding) or property does not exist");
+            commandIO.getPrintStream().println("Relay already exists (If you were adding) or property does not exist");
         }
     }
 }
