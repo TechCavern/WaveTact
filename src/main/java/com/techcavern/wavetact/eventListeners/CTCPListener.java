4c48f48c3628058f3d5d041432425cf7afb1e0ca
Julian Zhou <jzhou2oo6@gmail.com>
2018-01-27 12:56:34 -0600

    More work

--- src/main/java/com/techcavern/wavetact/eventListeners/CTCPListener.java
+++ src/main/java/com/techcavern/wavetact/eventListeners/CTCPListener.java
@@ -16,8 +16,7 @@ import org.jooq.Record;
 import org.pircbotx.Channel;
 import org.pircbotx.Colors;
 import org.pircbotx.hooks.ListenerAdapter;
-import org.pircbotx.hooks.events.NoticeEvent;
-import org.pircbotx.hooks.events.PrivateMessageEvent;
+import org.pircbotx.hooks.events.*;
 
 import static com.techcavern.wavetactdb.Tables.NETWORKPROPERTY;
 
@@ -28,6 +27,63 @@ public class CTCPListener extends ListenerAdapter {
 
     @Override
     public void onPing(PingEvent event) throws Exception {
+        class process implements Runnable {
+            public void run() {
+                if(event.getChannel() == null){
+                    IRCUtils.sendLogChanMsg(event.getBot(), "[PM] " +IRCUtils.noPing(event.getUserHostmask().getNick()) +"!" + event.getUserHostmask().getLogin()+ "@" + event.getUserHostmask().getHostname() +":PING ");
+                }
+            }
+
+        }
+        Registry.threadPool.execute(new process());
+    }
+        @Override
+    public void onTime(TimeEvent event) throws Exception {
+        class process implements Runnable {
+            public void run() {
+                if(event.getChannel() == null){
+                    IRCUtils.sendLogChanMsg(event.getBot(), "[PM] " +IRCUtils.noPing(event.getUserHostmask().getNick()) +"!" + event.getUserHostmask().getLogin()+ "@" + event.getUserHostmask().getHostname() +":TIME ");
+                }
+            }
+
+        }
+        Registry.threadPool.execute(new process());
+    }    @Override
+    public void onFinger(FingerEvent event) throws Exception {
+        class process implements Runnable {
+            public void run() {
+                if(event.getChannel() == null){
+                    IRCUtils.sendLogChanMsg(event.getBot(), "[PM] " +IRCUtils.noPing(event.getUserHostmask().getNick()) +"!" + event.getUserHostmask().getLogin()+ "@" + event.getUserHostmask().getHostname() +":FINGER ");
+                }
+            }
+
+        }
+        Registry.threadPool.execute(new process());
+    }    @Override
+    public void onAction(ActionEvent event) throws Exception {
+        class process implements Runnable {
+            public void run() {
+                if(event.getChannel() == null){
+                    IRCUtils.sendLogChanMsg(event.getBot(), "[PM] " +IRCUtils.noPing(event.getUserHostmask().getNick()) +"!" + event.getUserHostmask().getLogin()+ "@" + event.getUserHostmask().getHostname() +":ACTION ");
+                }
+            }
+
+        }
+        Registry.threadPool.execute(new process());
+    }    @Override
+    public void onVersion(VersionEvent event) throws Exception {
+        class process implements Runnable {
+            public void run() {
+                if(event.getChannel() == null){
+                    IRCUtils.sendLogChanMsg(event.getBot(), "[PM] " +IRCUtils.noPing(event.getUserHostmask().getNick()) +"!" + event.getUserHostmask().getLogin()+ "@" + event.getUserHostmask().getHostname() +":VERSION ");
+                }
+            }
+
+        }
+        Registry.threadPool.execute(new process());
+    }
+        @Override
+    public void onUnknownEvent(UnknownEvent event) throws Exception {
         class process implements Runnable {
             public void run() {
                 if(event.getChannel() == null){
